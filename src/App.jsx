260d24a526288cc228adfab23f3eeca3ca3323e6
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2025-09-01 22:24:54 +0600

    first commit, added tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-3xl font-bold text-blue-950 ">
+          cansat taring kit dashboard
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
